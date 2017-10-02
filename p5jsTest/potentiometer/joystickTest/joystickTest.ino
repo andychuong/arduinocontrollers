b75e5eccf965872c80fc34c39909f9fe5f80f966
Andy Chuong <andy.chuong@colorado.edu>
2017-10-02 11:35:15 -0600

    p5 seral working part 1

---
+++ p5jsTest/potentiometer/joystickTest/joystickTest.ino
@@ -0,0 +1,12 @@
+void setup() {
+ Serial.begin(9600); // initialize serial communications
+}
+ 
+void loop() {
+ int horiz = analogRead(A0);// read the input pin
+ int vert = analogRead(A1);
+ int mappedH = map(horiz, 0, 1023, 0, 255); // remap the pot value to fit in 1 byte
+ int mappedV = map(vert, 0, 1023, 0, 255);
+ Serial.write(mappedH + mappedV);                             // print it out the serial port
+ delay(1);                                            // slight delay to stabilize the ADC
+}
