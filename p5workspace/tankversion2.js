a64772d17eec25d0402b598789ffd9dffb9a1005
root <root@DESKTOP-O11PBR3.localdomain>
2017-11-14 03:47:12 -0700

    rotations

--- p5workspace/tankversion2.js
+++ p5workspace/tankversion2.js
@@ -56,7 +56,10 @@ function setup() {
   // Player 1
   ready1 = false;
   player1 = createSprite(400, 200, 50, 50);
-  player1.shapeColor = 'blue';
+  // player1.shapeColor = 'blue';
+  blueTank = loadImage("assets/bluetank_small.png");
+  blueTank2 = loadImage("assets/bluetank_small2.png");
+  player1.addImage(blueTank);
   x1 = 90;
   y1 = 140;
   points1 = 0;
@@ -74,7 +77,7 @@ function setup() {
   points2 = 0;
   bullets2 = new Group();
 
-
+  heart = loadImage("assets/heart.png");
 }
 
 //Draw Function -----------------------
@@ -116,29 +119,29 @@ function draw() {
     player2.position.y = y2;
 
     // Player 2 Shooting @@@@@@@@@@@
-      if(keyWentDown("f")){
+      if(keyWentDown("h")){
         // console.log('c pressed');
-      //   var bullet2 = createSprite(player2.position.x, player2.position.y, 10, 10);
-      //   bullet2.setSpeed(10+player2.getSpeed(), player2.rotation);
-      //   bullet2.life = 160;
-      //   bullet2.shapeColor = 'black';
-      //   bullets2.add(bullet2);
+        var bullet2 = createSprite(player2.position.x, player2.position.y, 10, 10);
+        bullet2.setSpeed(10+player2.getSpeed(), player2.rotation);
+        bullet2.life = 160;
+        bullet2.shapeColor = 'black';
+        bullets2.add(bullet2);
       }
 
       //detect hits
-    // bullets1.overlap(player2, oneHitTwo);
-    // bullets2.overlap(player1, twoHitOne);
+    bullets1.overlap(player2, oneHitTwo);
+    bullets2.overlap(player1, twoHitOne);
 
     //draw hearts
     hearts1 = wins - points2;
     hearts2 = wins - points1;
 
     for(var i = 0; i < hearts1; i++){
-      // image(heart, 50 + (22*i), windowHeight - 48);
+      image(heart, 50 + (22*i), windowHeight - 48);
     }
 
     for(var j = 0; j < hearts2; j++){
-      // image(heart, windowWidth - (70 + (22*j) ), windowHeight - 48);
+      image(heart, windowWidth - (70 + (22*j) ), windowHeight - 48);
     }
     
 
@@ -204,20 +207,28 @@ function serialEvent() {
 }
 
 function updateValues(){ // update sketch.js variables with sensor data
+  console.log(player1.rotation);
+  //> 0
+  // < 180
+  // down 90
+  // up 270
   //// Player 1 ------------
   // X1
   //left
   if(sensorX1 < 470){
     if(x1 > 90){
       x1-=3;
+
       // console.log(x1);
     }
+    // player1.rotation -= 4;
   }//right
   if(sensorX1 > 530){
     if(x1 < windowWidth/2 - 45){
       x1+=3;
       // console.log(x1);
     }
+    // player1.rotation += 4;
   }
   
   // Y1
@@ -226,15 +237,76 @@ function updateValues(){ // update sketch.js variables with sensor data
     if(y1 < windowHeight - 90){
       y1 += 3;
       // console.log(y1);
+      // player1.addSpeed(-.2, player1.rotation);
+      // console.log(player1.rotation);
     }
   }//up
   if(sensorY1 < 470){
+
     if(y1 > 140){
       y1 -= 3;
       // console.log(y1);
+      // player1.addSpeed(.2, player1.rotation);
     }
   }
   // Rotation 1
+  //up
+  if(sensorY1 < 470){ // check up greater than 470, less than 530 -> between 470 and 530
+    if(sensorX1 > 470){ // check not left
+      if(sensorX1 < 530){ //check not right
+        player1.rotation = 270;
+      }
+    }
+  }
+  //down
+  if(sensorY1 > 530){ // check down greater than 470, less than 530 -> between 470 and 530
+    if(sensorX1 > 470){ // check not left
+      if(sensorX1 < 530){ //check not right
+        player1.rotation = 90;
+      }
+    }
+  }
+  //right
+  if(sensorX1 > 530){ // check right
+    if(sensorY1 < 530){ //check not down
+      if(sensorY1 > 470){ //check not up
+        player1.rotation = 0;
+      }
+    }
+  }
+  //left
+  if(sensorX1 < 470){ // check right
+    if(sensorY1 < 530){ //check not down
+      if(sensorY1 > 470){ //check not up
+        player1.rotation = 180;
+      }
+    }
+  }
+  //top right
+  if(sensorY1 < 470){ //up
+    if(sensorX1 > 530){ //right
+      player1.rotation = 315;
+    }
+  }
+  //bottom right
+  if(sensorY1 > 530){ //up
+    if(sensorX1 > 530){ //right
+      player1.rotation = 45;
+    }
+  }
+  //bottom left
+  if(sensorY1 > 530){ //down
+    if(sensorX1 < 470){ //left
+      player1.rotation = 135;
+    }
+  }
+  //top left
+  if(sensorY1 < 470){ //up
+    if(sensorX1 < 470){ //left
+      player1.rotation = 225;
+    }
+  }
+
   // rotation1 = map(sensorR1, 0, 1023, -90, 90);
   // player1.rotation = rotation1;
 
@@ -245,14 +317,17 @@ function updateValues(){ // update sketch.js variables with sensor data
   if(sensorX2 < 470){
     if(x2 > windowWidth/2 + 45){
       x2 -= 3;
+
       // console.log(x2);
     }
+    // player2.rotation -= 4;
   }
   // right
   if(sensorX2 > 530){
     if(x2 < windowWidth - 90){
       x2 += 3;
     }
+    // player2.rotation += 4;
   }
   // Y2
   // down
@@ -270,10 +345,77 @@ function updateValues(){ // update sketch.js variables with sensor data
     }
   }
   // Rotation 2
+  //up
+  if(sensorY2 < 470){ // check up greater than 470, less than 530 -> between 470 and 530
+    if(sensorX2 > 470){ // check not left
+      if(sensorX2 < 530){ //check not right
+        player2.rotation = 270;
+      }
+    }
+  }
+  //down
+  if(sensorY2 > 530){ // check down greater than 470, less than 530 -> between 470 and 530
+    if(sensorX2 > 470){ // check not left
+      if(sensorX2 < 530){ //check not right
+        player2.rotation = 90;
+      }
+    }
+  }
+  //right
+  if(sensorX2 > 530){ // check right
+    if(sensorY2 < 530){ //check not down
+      if(sensorY2 > 470){ //check not up
+        player2.rotation = 0;
+      }
+    }
+  }
+  //left
+  if(sensorX2 < 470){ // check right
+    if(sensorY2 < 530){ //check not down
+      if(sensorY2 > 470){ //check not up
+        player2.rotation = 180;
+      }
+    }
+  }
+  //top right
+  if(sensorY2 < 470){ //up
+    if(sensorX2 > 530){ //right
+      player2.rotation = 315;
+    }
+  }
+  //bottom right
+  if(sensorY2 > 530){ //up
+    if(sensorX2 > 530){ //right
+      player2.rotation = 45;
+    }
+  }
+  //bottom left
+  if(sensorY2 < 470){ //down
+    if(sensorX2 < 470){ //left
+      player2.rotation = 225;
+    }
+  }
+  //top left
+  if(sensorY2 > 530){ //down
+    if(sensorX2 < 470){ //left
+      player2.rotation = 135;
+    }
+  }
   // rotation2 = map(sensorR2, 0, 1023, 270, 90);
-  // player2.rotation = rotation2;
+  // player2.rotation = rotation2;cccccccccc
   // console.log(rotation2);
 
+}
+
+function oneHitTwo(bullet1){
+  // player2.shapeColor = "orange";
+  points1++;
+  bullet1.remove();
+}
+
+function twoHitOne(bullet2){
+  points2++;
+  bullet2.remove();
 }
 
 // get the list of ports:
