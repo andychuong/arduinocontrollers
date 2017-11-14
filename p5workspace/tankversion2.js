f122dd26f02f11068369d7e6d101ae5ef32db24a
root <root@DESKTOP-O11PBR3.localdomain>
2017-11-14 04:34:04 -0700

    game states

--- p5workspace/tankversion2.js
+++ p5workspace/tankversion2.js
@@ -37,6 +37,11 @@ var gameState;
 // 1 = in progress
 // 2 = end 
 
+function preload() {
+   fontTitle = loadFont("assets/Bangers-Regular.ttf");
+   fontOther = loadFont("assets/SquadaOne-Regular.ttf");
+}
+
 
 //Setup Function ----------------------
 function setup() {
@@ -51,6 +56,7 @@ function setup() {
   serial.list();                      // list the serial ports
   serial.open(portName);              // open a serial port
 
+  gameState = 0;
   wins = 5;
 
   // Player 1
@@ -58,7 +64,7 @@ function setup() {
   player1 = createSprite(400, 200, 50, 50);
   // player1.shapeColor = 'blue';
   blueTank = loadImage("assets/bluetank_small.png");
-  blueTank2 = loadImage("assets/bluetank_small2.png");
+  // blueTank2 = loadImage("assets/bluetank_small2.png");
   player1.addImage(blueTank);
   x1 = 90;
   y1 = 140;
@@ -82,79 +88,195 @@ function setup() {
 
 //Draw Function -----------------------
 function draw() {
-  background('white');
+
+    // GAME STATE 0 ----- START SCREEN ----- ----- 
+  if(gameState == 0){
+    background('white');
 
     fill(200);
     textSize(80);
-    // textFont(fontTitle);
+    textFont(fontTitle);
     text("Battle Tanks", windowWidth/2 -200, 70);
 
-    //rectangle box
+    rect(100,100, windowWidth - 200, windowHeight - 200);
+
+    fill(0);
+    textSize(40);
+    textFont(fontOther);
+    text("Instructions", windowWidth/2 - 90, 135);
+    rect(windowWidth/2 - 300, 145, 600, 5);
+
+    textSize(30);
+    text("- Use the joystick to move", windowWidth/2 - 140, 180);
+    text("- Press the blue button to reload", windowWidth/2 - 140, 210 );
+    text("- Press the red button to reload", windowWidth/2 - 140, 240);
+
+    textSize(40);
+    text("To ready up, press the blue button.", windowWidth/2 -250, windowHeight/2);
+    text("When both players are ready, the game will start!", windowWidth/2 - 335, windowHeight/2 + 40);
+
+    image(blueTank, 210, windowHeight - 300);
+    image(greenTank2, windowWidth-280, windowHeight - 300);
+
+    if(points1 != 0 || points2 != 0){
+      points1 = 0;
+      points2 = 0;
+      ready1 = false;
+      ready2 = false;
+      // console.log(points2);
+      // console.log(points1);
+    }
+
+    if(ready1){
+      text("Player 1: Ready!", 120, windowHeight - 200);
+      // console.log("ready1");
+    }
+    if(ready2){
+      text("Player 2: Ready!", windowWidth - 360, windowHeight - 200);
+      // console.log("ready2");
+    }
+
+    // rect(windowWidth/2 - 2, 0, 4, windowHeight);
+    // console.log(ready1);c
+    // console.log(ready2);
+
+    if(keyWentDown("c")){
+      ready1 = true;
+      // console.log(gameState);
+    }
+    if(keyWentDown("h")){
+      ready2 = true;
+      // console.log(gameState)
+    }
+    if(ready1 == true && ready2 == true){
+      gameState = 1;
+    }
+  }
+
+  // GAME STATE 1 ----- GAME PLAY ----- -----
+  if(gameState == 1){
+    background('white');
+
+      fill(200);
+      textSize(80);
+      textFont(fontTitle);
+      text("Battle Tanks", windowWidth/2 -200, 70);
+
+      //rectangle box
+      stroke(0);
+      strokeWeight(2);
+      rect(50,100,windowWidth - 100, windowHeight - 150);
+      strokeWeight(1);
+
+      //middle bar
+      fill(0);
+      rect(windowWidth/2 -10,100, 20, windowHeight - 150);
+
+      
+      // Player 1 Location
+      player1.position.x = x1;
+      player1.position.y = y1;
+
+      // Player 1 Shooting @@@@@@@@@@@
+      if(keyWentDown("c")){
+        console.log('1 shoot');
+          var bullet1 = createSprite(player1.position.x, player1.position.y, 10, 10);
+          bullet1.setSpeed(10+player1.getSpeed(), player1.rotation);
+          bullet1.life = 160;
+          bullet1.shapeColor = 'black';
+          bullets1.add(bullet1);
+      }
+       
+        // Player 2 Location
+      player2.position.x = x2;
+      player2.position.y = y2;
+
+      // Player 2 Shooting @@@@@@@@@@@
+        if(keyWentDown("h")){
+          // console.log('c pressed');
+          var bullet2 = createSprite(player2.position.x, player2.position.y, 10, 10);
+          bullet2.setSpeed(10+player2.getSpeed(), player2.rotation);
+          bullet2.life = 160;
+          bullet2.shapeColor = 'black';
+          bullets2.add(bullet2);
+        }
+
+        //detect hits
+      bullets1.overlap(player2, oneHitTwo);
+      bullets2.overlap(player1, twoHitOne);
+
+      //draw hearts
+      hearts1 = wins - points2;
+      hearts2 = wins - points1;
+
+      for(var i = 0; i < hearts1; i++){
+        image(heart, 50 + (22*i), windowHeight - 48);
+      }
+
+      for(var j = 0; j < hearts2; j++){
+        image(heart, windowWidth - (70 + (22*j) ), windowHeight - 48);
+      }
+      
+
+      // Check Win
+
+      if(points1 == wins || points2 == wins){
+        gameState = 2;
+
+      }
+
+      // Update Values
+      drawSprites();
+      updateValues();
+    }
+
+    // GAME STATE 2 ----- GAME OVER ----- ----- 
+
+  if(gameState == 2){
+    background('white');
+
+    fill(200);
+    textSize(80);
+    textFont(fontTitle);
+    text("Battle Tanks", windowWidth/2 -200, 70);
+
     stroke(0);
     strokeWeight(2);
     rect(50,100,windowWidth - 100, windowHeight - 150);
     strokeWeight(1);
 
-    //middle bar
-    fill(0);
-    rect(windowWidth/2 -10,100, 20, windowHeight - 150);
+    textFont(fontOther);
+    textSize(200)
 
-    
-    // Player 1 Location
-    player1.position.x = x1;
-    player1.position.y = y1;
-
-    // Player 1 Shooting @@@@@@@@@@@
-    if(keyWentDown("c")){
-      console.log('1 shoot');
-        var bullet1 = createSprite(player1.position.x, player1.position.y, 10, 10);
-        bullet1.setSpeed(10+player1.getSpeed(), player1.rotation);
-        bullet1.life = 160;
-        bullet1.shapeColor = 'black';
-        bullets1.add(bullet1);
-      }
-     
-      // Player 2 Location
-    player2.position.x = x2;
-    player2.position.y = y2;
-
-    // Player 2 Shooting @@@@@@@@@@@
-      if(keyWentDown("h")){
-        // console.log('c pressed');
-        var bullet2 = createSprite(player2.position.x, player2.position.y, 10, 10);
-        bullet2.setSpeed(10+player2.getSpeed(), player2.rotation);
-        bullet2.life = 160;
-        bullet2.shapeColor = 'black';
-        bullets2.add(bullet2);
-      }
-
-      //detect hits
-    bullets1.overlap(player2, oneHitTwo);
-    bullets2.overlap(player1, twoHitOne);
-
-    //draw hearts
-    hearts1 = wins - points2;
-    hearts2 = wins - points1;
-
-    for(var i = 0; i < hearts1; i++){
-      image(heart, 50 + (22*i), windowHeight - 48);
+    if(points1 == wins){
+      fill("#2262c9");
+      text("Player 1 Wins!", windowWidth/2 - 500, windowHeight/2);
+    }
+    if(points2 == wins){
+      fill("#0eca29");
+      text("Player 2 Wins!", windowWidth/2 - 500, windowHeight/2);
     }
 
-    for(var j = 0; j < hearts2; j++){
-      image(heart, windowWidth - (70 + (22*j) ), windowHeight - 48);
+    fill("black");
+    textSize(40);
+    text("To restart, press the blue button.", windowWidth/2 -250, windowHeight/2+80);
+
+    if(sensorS1 == 1){
+      ready1 = false;
     }
-    
-
-    // Check Win
-
-    if(points1 == wins || points2 == wins){
-      gameState = 2;
-
+    if(sensorS2 == 1){
+      ready2 = false;
     }
 
-    // Update Values
-    drawSprites();
-    updateValues();
+    if(ready1 == false || ready2 == false){
+      // console.log(points1);
+      // console.log(points2);
+      ready1 = false;
+      ready2 = false;
+      gameState = 0;
+      
+    }
+  }
 }
 
   //Guide 1:
@@ -170,7 +292,7 @@ function draw() {
   //A4 ~ [4]: Verti (y)
   //D7 ~ [11]: Joystick Button - ??
   //D8 ~ [12]: Small Buttom ( - )
-  //D9 ~ [13]: Small Button (Reset)
+  //D9 ~ [13]: Blue Button (Shoot)
   //D10 ~ [14]: Red Button (Reload)
 
 //Interpret serial data here ----------
@@ -188,17 +310,17 @@ function serialEvent() {
       //Player 1:
       sensorX1 = sensors[0];
       sensorY1 = sensors[1];
-      // console.log(sensors);
+      console.log(sensors);
       sensorS1 = sensors[8];
       // console.log(sensors[8]);
       // console.log(sensors);
 
 
-      
-
       //Player 2:
       sensorX2 = sensors[3];
       sensorY2 = sensors[4];
+
+      sensorS2 = sensors[13]
 
       }
     }
