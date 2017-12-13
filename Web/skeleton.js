4f69bdc0d5b3894d1c7b125079d88ae8f3d39511
Andy Chuong <andy.chuong@colorado.edu>
2017-12-13 12:58:45 -0700

    Web folder

---
+++ Web/skeleton.js
@@ -0,0 +1,76 @@
+//Serial Variables
+var serial;          // variable to hold an instance of the serialport library
+var portName = 'COM4';  // fill in your serial port name here
+
+//Global Game Variables ---------------
+
+
+//Setup Function ----------------------
+function setup() {
+  createCanvas(640, 480);
+  serial = new p5.SerialPort();       // make a new instance of the serialport library
+  serial.on('list', printList);       // set a callback function for the serialport list event
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen);        // callback for the port opening
+  serial.on('data', serialEvent);     // callback for when new data arrives
+  serial.on('error', serialError);    // callback for errors
+  serial.on('close', portClose);      // callback for the port closing
+  serial.list();                      // list the serial ports
+  serial.open(portName);              // open a serial port
+}
+
+//Draw Function -----------------------
+function draw() {
+ background(0); // black background
+}
+
+//Interpret serial data here ----------
+function serialEvent() {
+  // read a string from the serial port
+  // until you get carriage return and newline:
+  var inString = serial.readStringUntil('\r\n');
+  //check to see that there's actually a ssetring there:
+  if (inString.length > 0) {
+    if (inString !== 'hello') {           // if you get hello, ignore it
+      var sensors = split(inString, ','); // split the string on the commas
+      if (sensors.length > 16) {           // if there are sixteen elements (6 analog, 10 digital)
+      //Use sensor data here:
+
+
+      }
+    }
+    serial.write('x'); // send a byte requesting more serial data 
+  }
+}
+
+// get the list of ports:
+function printList(portList) {
+ // portList is an array of serial port names
+ for (var i = 0; i < portList.length; i++) {
+ // Display the list the console:
+ print(i + " " + portList[i]);
+ }
+}
+ 
+function serverConnected() {
+ print('connected to server.');
+}
+ 
+function portOpen() {
+ print('the serial port opened.')
+}
+ 
+function serialError(err) {
+ print('Something went wrong with the serial port. ' + err);
+}
+ 
+function portClose() {
+ print('The serial port closed.');
+}
+
+function closingCode(){
+  serial.close(portName);
+  return null;
+}
+
+window.onbeforeunload = closingCode;
